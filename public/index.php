239d5cb710e456d743d18f15091a32eae0dfe5e9
Kaga Akatsuki <filberttkarry2210@gmail.com>
2024-04-19 22:14:44 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-putenv("APP_ENV=production");
+// putenv("APP_ENV=production");
 
 /*
 |--------------------------------------------------------------------------
